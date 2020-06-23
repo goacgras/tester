0520251fac6f4049eb279c39ebedf6bf559125e5
unknown <fachrezaghifary@gmail.com>
2020-06-23 14:13:59 +0700

    making branch

--- cats.js
+++ cats.js
@@ -1,16 +1,10 @@
-console.log("change 1");console.log("This is my app");
-console.log("change 2");
-console.log("change 3");
-console.log("fachreza");console.log("This is my app");
-console.log("change 2");
-console.log("change 3");
-console.log("fachreza");console.log("This is my app");
-console.log("change 2");
-console.log("change 3");
-console.log("fachreza");console.log("This is my app");
-console.log("change 2");
-console.log("change 3");
-console.log("fachreza");console.log("This is my app");
-console.log("change 2");
-console.log("change 3");
-console.log("fachreza");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+console.log("INI ADALAH BRANCH GIT");
+
